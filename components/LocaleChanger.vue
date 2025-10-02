9efb723b0cfeba96bf5b82d4708bf25a342e1583
Martin Klomp <m.klomp@tudelft.nl>
2025-10-02 19:28:55 +0200

    Fix for not loading all flags

--- components/LocaleChanger.vue
+++ components/LocaleChanger.vue
@@ -1,14 +1,15 @@
 <script setup>
 import { useI18n } from 'vue-i18n'
 import { useSwitchLocalePath } from '#i18n'
-import 'flag-icons/css/flag-icons.min.css'
+import nlFlag from 'flag-icons/flags/4x3/nl.svg?url'
+import gbFlag from 'flag-icons/flags/4x3/gb.svg?url'
 
 const { locale } = useI18n()
 const switchLocalePath = useSwitchLocalePath()
 
 const langs = [
-  { code: "en", text: "English", flag: "gb" },
-  { code: "nl", text: "Nederlands", flag: "nl" },
+  { code: "en", text: "English", flag: gbFlag },
+  { code: "nl", text: "Nederlands", flag: nlFlag },
 ]
 </script>
 
@@ -19,14 +20,35 @@ const langs = [
      role="button"
      data-bs-toggle="dropdown"
      aria-expanded="false">
-    <span :class="`fi fi-${langs.find(l => l.code === locale).flag}`"></span>
+    <img class="fi" :src="langs.find(l => l.code === locale).flag" alt="flag">
   </a>
   <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="localeDropdown">
     <li v-for="lang in langs" :key="lang.code">
       <NuxtLink class="dropdown-item" :to="switchLocalePath(lang.code)">
-        <span :class="`fi fi-${lang.flag}`"></span>
+        <img class="fi" :src="`${lang.flag}`" alt="flag">
         {{ lang.text }}
       </NuxtLink>
     </li>
   </ul>
 </template>
+
+<style scoped>
+.fib, .fi {
+  background-size: contain;
+  background-position: 50%;
+  background-repeat: no-repeat;
+}
+
+.fi {
+  position: relative;
+  display: inline-block;
+  width: 1.333333em;
+  line-height: 1em;
+}
+.fi:before {
+  content: " ";
+}
+.fi.fis {
+  width: 1em;
+}
+</style>
